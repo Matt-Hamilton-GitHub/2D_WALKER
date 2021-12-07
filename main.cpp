1ed2d8759df36e40de6c4e0a3aab5f953c477d4c
Matt Hamilton <66541468+Matt-Hamilton-GitHub@users.noreply.github.com>
2021-12-07 22:33:11 +0000

    final version

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
-// Write a function that simulates someone’s two-dimensional random walk along an M by N grid where M and N are function parameters. This program is a generalization of the one-dimensional random walk. The walker moves in one of four directions. The walker chooses the direction randomly. The walker cannot move off the grid. The function ends when the walker visits every grid position at least once. The function returns a dynamic structure that is an ordered record of the walker’s positions over time. The first element of the structure is the walker’s first position. The last element is the walker’s last position.
 
+// Write a function that simulates someone’s two-dimensional random walk along an M by N grid where M and N are function parameters. This program is a
+// generalization of the one-dimensional random walk. The walker moves in one of four directions. The walker chooses the direction randomly. The walker cannot move off the grid. 
+// The function ends when the walker visits every grid position at least once. The function returns a dynamic structure that is an ordered record of the walker’s positions over time. 
+// The first element of the structure is the walker’s first position. The last element is the walker’s last position.
 
 
 #include <stdio.h>
@@ -8,93 +11,155 @@
 #include <iostream>
 using namespace std;
 
-
-int get_walk(int N)
-{
-    int walk = 0;
-    int toss;
-    for (int i = 0; i < N-1; i++)
-    {
-        toss = rand() % 2;
-        if (toss == 0)
-        {
-            // head
-            walk += 1;
-        }
-        else
-        {
-            walk += -1;
-        }
-    }
-    return walk;
-}
 vector<pair<int, int>> random_walk(int M, int N)
 {
     vector<pair<int, int>> moves;
     // int max = N*N;
     int iter = 0;
-    vector<vector<int>> walk_m(M, vector<int>(N, -1));
+    vector<vector<int>> walk_m(M, vector<int>(N, 0));
     int move, walk, walk_i, walk_j;
     int i, j;
     moves.push_back(make_pair(0, 0));
     iter++;
     walk_m[0][0] = 1;
 
-    int curr = 0;
     int curr_i = 0;
     int curr_j = 0;
 
     while (iter != M * N)
     {
-        move = rand() % 3;
-        if (move == 0)
-        {
-            // pick x axis
-            walk = get_walk(M);
 
-            if (curr_i + walk < M && curr_i + walk >= 0)
+        move = rand() % 8;
+        switch (move)
+        {
+        case 0:
+            /* north */
+            i = curr_i - 1;
+            j = curr_j;
+            if (i >= 0)
             {
-                curr_i = curr_i + walk;
-                if (walk_m[curr_i][curr_j] == -1)
+                curr_i = i;
+                if (walk_m[curr_i][curr_j] == 0)
                     iter++;
 
-                walk_m[curr_i][0] = 1;
-                // cout << "curr: " << curr_i << ", " << curr_j << endl;
-                moves.push_back(make_pair(curr_i, curr_j));
-            }
-        }
-        else if (move == 1)
-        {
-            // pick y axis
-            walk = get_walk(N);
-            if (curr_j + walk < N && curr_j + walk >= 0)
-            {
-                curr_j = curr_j + walk;
-                if (walk_m[curr_i][curr_j] == -1)
-                    iter++;
                 walk_m[curr_i][curr_j] = 1;
-                // cout << "curr: " << curr_i << ", " << curr_j << endl;
                 moves.push_back(make_pair(curr_i, curr_j));
             }
-        }
-        else
-        {
-            // pick both
-            walk_i = get_walk(M);
-            walk_j = get_walk(N);
-            if ((curr_i + walk_i < M && curr_i + walk_i >= 0) && (curr_j + walk_j < N && curr_j + walk_j >= 0))
+
+            break;
+        case 1:
+            /* south */
+            i = curr_i;
+            j = curr_j - 1;
+            if (j >= 0)
             {
-                curr_i = curr_i + walk_i;
-                curr_j = curr_j + walk_j;
-                if (walk_m[curr_i][curr_j] == -1)
+                curr_j = j;
+                if (walk_m[curr_i][curr_j] == 0)
                     iter++;
+
                 walk_m[curr_i][curr_j] = 1;
-                // cout << "curr: " << curr_i << ", " << curr_j << endl;
                 moves.push_back(make_pair(curr_i, curr_j));
             }
+
+            break;
+        case 2:
+            /* east */
+            i = curr_i;
+            j = curr_j + 1;
+            if (j < N)
+            {
+                curr_j = j;
+                if (walk_m[curr_i][curr_j] == 0)
+                    iter++;
+
+                walk_m[curr_i][curr_j] = 1;
+                moves.push_back(make_pair(curr_i, curr_j));
+            }
+
+            break;
+        case 3:
+            /* west */
+            i = curr_i + 1;
+            j = curr_j;
+            if (i < M)
+            {
+                curr_i = i;
+                if (walk_m[curr_i][curr_j] == 0)
+                    iter++;
+
+                walk_m[curr_i][curr_j] = 1;
+                moves.push_back(make_pair(curr_i, curr_j));
+            }
+
+            break;
+        case 4:
+            /* NE */
+            i = curr_i - 1;
+            j = curr_j + 1;
+            if (i >= 0 && j < N)
+            {
+                curr_j = j;
+                curr_i = i;
+                if (walk_m[curr_i][curr_j] == 0)
+                    iter++;
+
+                walk_m[curr_i][curr_j] = 1;
+                moves.push_back(make_pair(curr_i, curr_j));
+            }
+            break;
+        case 5:
+            /* NW */
+            i = curr_i - 1;
+            j = curr_j - 1;
+            if (i >= 0 && j >= 0)
+            {
+                curr_j = j;
+                curr_i = i;
+                if (walk_m[curr_i][curr_j] == 0)
+                    iter++;
+
+                walk_m[curr_i][curr_j] = 1;
+                moves.push_back(make_pair(curr_i, curr_j));
+            }
+
+            break;
+        case 6:
+            /* SW */
+            i = curr_i + 1;
+            j = curr_j - 1;
+            if (j >= 0 && i < M)
+            {
+                curr_j = j;
+                curr_i = i;
+                if (walk_m[curr_i][curr_j] == 0)
+                    iter++;
+
+                walk_m[curr_i][curr_j] = 1;
+                moves.push_back(make_pair(curr_i, curr_j));
+            }
+
+            break;
+        case 7:
+            /* SE */
+            i = curr_i + 1;
+            j = curr_j + 1;
+            if (i < M && j < N)
+            {
+                curr_j = j;
+                curr_i = i;
+                if (walk_m[curr_i][curr_j] == 0)
+                    iter++;
+
+                walk_m[curr_i][curr_j] = 1;
+                moves.push_back(make_pair(curr_i, curr_j));
+            }
+            break;
+        default:
+            break;
         }
+
     }
-    
+
     return moves;
 }
 int main()
@@ -102,7 +167,7 @@ int main()
     int M, N;
     vector<pair<int, int>> moves;
     cout << "Enter M: ";
-    cin >> M; 
+    cin >> M;
     cout << "Enter N: ";
     cin >> N;
     moves = random_walk(M, N);
@@ -110,4 +175,7 @@ int main()
     {
         cout << "Move: " << i.first << ", " << i.second << endl;
     }
+	
+	system("pause");
+	return 0;
 }
