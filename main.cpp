a65193dfa888b6e8f7c1ada29996d672f1612450
Matt Hamilton <66541468+Matt-Hamilton-GitHub@users.noreply.github.com>
2021-12-07 01:08:02 +0000

    INITIATED

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
 // Write a function that simulates someone’s two-dimensional random walk along an M by N grid where M and N are function parameters. This program is a generalization of the one-dimensional random walk. The walker moves in one of four directions. The walker chooses the direction randomly. The walker cannot move off the grid. The function ends when the walker visits every grid position at least once. The function returns a dynamic structure that is an ordered record of the walker’s positions over time. The first element of the structure is the walker’s first position. The last element is the walker’s last position.
+
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+//Main function definition
+int main() {
+	
+  system("pause");
+  return 0;
+}
